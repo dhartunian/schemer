b8200715024e1f563fb420de35e5258c5f012152
David Hartunian <dhartunian@gmail.com>
2012-01-16 22:22:41 -0500

    starting over

---
+++ chapter1.scm
@@ -0,0 +1,46 @@
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat)))))))
+
+
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) a) (cdr lat))
+      (else (cons (car lat)
+                  (rember a (cdr lat)))))))
+
+(define firsts
+  (lambda (l)
+    (cond ((null? l) (quote ()))
+          (else (cons (car (car l))
+                      (firsts (cdr l)))))))
+
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? old (car lat)) (cons old 
+                                 (cons new 
+                                       (cdr lat))))
+      (else (cons (car lat) 
+                  (insertR new old (cdr lat)))))))
+
+
+                     
