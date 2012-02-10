22086cf650e81b104cb380ab40dbfe837010ef2a
David Hartunian <dhartunian@gmail.com>
2012-02-09 21:17:55 -0500

    finished chapter 3

--- chapter3.scm
+++ chapter3.scm
@@ -50,3 +50,54 @@
      (else (cons (car lat)
 		 (subst new old (cdr lat)))))))
 
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+     ((null? lat) '())
+     ((or (eq? (car lat) o1) (eq? (car lat) o2))
+      (cons new (cdr lat)))
+     (else (cons (car lat)
+		 (subst2 new o1 o2 (cdr lat)))))))
+
+(define multirember
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     ((eq? a (car lat))
+      (multirember (cdr lat)))
+     (else (cons (car lat)
+		 (multirember a lat))))))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     ((eq? old (car lat))
+      (cons old (cons new
+		      (multiinsertR new old (cdr lat)))))
+     (else (cons (car lat)
+		 (multiinsertR new old (cdr lat)))))))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     ((eq? old (car lat))
+	  (cons new (cons old
+			  (multiinsertL new old (cdr lat)))))
+     (else (cons (car lat)
+		 (multiinsertL new old (cdr lat)))))))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     ((eq? old (car lat))
+      (cons new
+	    (multisubst new old (cdr lat))))
+     (else (cons (car lat)
+		 (multisubst new old (cdr lat)))))))
+
+
+		   
+      
