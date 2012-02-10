96aafb2f75fd73c6452afb2c86244a0a987fd3ff
David Hartunian <dhartunian@gmail.com>
2012-02-09 20:46:43 -0500

    working on stuff

---
+++ chapter2.scm
@@ -0,0 +1,21 @@
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(define rember
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     ((eq? (car lat) a) (cdr lat))
+     (else (cons (car lat) 
+		 (rember a (cdr lat)))))))
+
+(define firsts
+  (
---
+++ chapter3.scm
@@ -0,0 +1,55 @@
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(define rember
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     ((eq? (car lat) a) (cdr lat))
+     (else (cons (car lat) 
+		 (rember a (cdr lat)))))))
+
+(define firsts
+  (lambda (lat)
+    (cond
+     ((null? lat) '())
+     (else (cons (car (car lat))
+		 (firsts (cdr lat)))))))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     ((eq? (car lat) old) (cons (car lat) 
+				(cons new
+				      (insertR new old (cdr lat)))))
+     (else (cons (car lat)
+		 (insertR new old (cdr lat)))))))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     ((eq? (car lat) old) (cons new 
+				(cons (car lat)
+				      (insertL new old (cdr lat)))))
+     (else (cons (car lat)
+		 (insertL new old (cdr lat)))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     ((eq? (car lat) old) (cons new 
+				(subst new old (cdr lat))))
+     (else (cons (car lat)
+		 (subst new old (cdr lat)))))))
+
